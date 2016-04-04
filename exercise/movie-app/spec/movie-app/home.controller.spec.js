c7d0a31295a736100cb8770ed6e68a6a0b7a0d42
gcroteau <guillaume.croteau.1@gmail.com>
2016-04-04 14:10:37 -0400

    Completed module 7

--- exercise/movie-app/spec/movie-app/home.controller.spec.js
+++ exercise/movie-app/spec/movie-app/home.controller.spec.js
@@ -16,29 +16,21 @@ describe('Home Controller', function() {
 	  ];
 	var $scope;
 	var $interval;
-	var omdbApi;
 
 	beforeEach(module('movieApp'));
 
-	beforeEach(inject(function(_$location_, _$interval_, _omdbApi_) {
-		$scope = {};
-		$interval = _$interval_;
-		omdbApi = _omdbApi_;
-	}));
-
 	beforeEach(inject(function(_$q_, _PopularMovies_) {
 		spyOn(_PopularMovies_, 'get').and.callFake(function() {
-	      var deferred = _$q_.defer();
-	      deferred.resolve(['tt0076759', 'tt0080684', 'tt0086190']);
-	      return deferred.promise;
-	    });
+			var deferred = _$q_.defer();
+			deferred.resolve(['tt0076759', 'tt0080684', 'tt0086190']);
+			return deferred.promise;
+		});
 	}));
 
-	beforeEach(inject(function(_$controller_, _$q_) {
-	    spyOn(omdbApi, 'find').and.callFake(function() {
-      		var deferred = _$q_.defer();
-      		var args = omdbApi.find.calls.mostRecent().args[0];
-
+	beforeEach(inject(function(_$q_, _omdbApi_) {
+		spyOn(_omdbApi_, 'find').and.callFake(function() {
+			var deferred = _$q_.defer();
+			var args = _omdbApi_.find.calls.mostRecent().args[0];
 			if (args === 'tt0076759') {
 				deferred.resolve(results[0]);
 			} else if (args === 'tt0080684') {
@@ -48,44 +40,43 @@ describe('Home Controller', function() {
 			} else {
 				deferred.reject();
 			}
-
-	      return deferred.promise;
-	    });
+			return deferred.promise;
+		});
 	}));
 
-	beforeEach(inject(function(_$controller_, _$rootScope_, _PopularMovies_) {
+	beforeEach(inject(function(_$controller_, _$interval_, _$rootScope_, _omdbApi_, _PopularMovies_) {
+		$scope = {};
+		$interval = _$interval_;
 		_$controller_('HomeController', { 
-			$scope: $scope, 
-			$interval_: $interval,
-			omdbApi: omdbApi,
+			$scope: $scope,
+			$interval: _$interval_,
+			omdbApi: _omdbApi_,
 			PopularMovies: _PopularMovies_
 		});
-
 		_$rootScope_.$apply();
 	}));
 
-
 	it('should rotate movies every 5 seconds', function() {
-		// should have a default starting movie
+		// should have a default movie
 		expect($scope.result.Title).toBe(results[0].Title);
-		
-		// after 5 seconds, should be next movie
+		// should update after 5 seconds
 		$interval.flush(5000);
 		expect($scope.result.Title).toBe(results[1].Title);
-		
-		// after 5 seconds, should be next movie
+		// should update after 5 seconds
 		$interval.flush(5000);
 		expect($scope.result.Title).toBe(results[2].Title);
-
-		// should go back to start
+		// should return to default
 		$interval.flush(5000);
 		expect($scope.result.Title).toBe(results[0].Title);
-
-		expect(omdbApi.find.calls.argsFor(0)).toEqual(['tt0076759']);
-    	expect(omdbApi.find.calls.argsFor(1)).toEqual(['tt0080684']);
-    	expect(omdbApi.find.calls.argsFor(2)).toEqual(['tt0086190']);
-    	expect(omdbApi.find.calls.argsFor(3)).toEqual(['tt0076759']);
-
 	});
 
 });
+
+
+
+
+
+
+
+
+
